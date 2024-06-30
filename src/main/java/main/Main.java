43d2a1e05a3c6d33fd509e91b7a26f8bbe44b30b
Popsonikk <popson2001@onet.pl>
2024-06-30 16:45:37 +0200

    Wstępna konfiguracja

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,24 @@
 package main;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("witaj");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader mainWindowLoader = new FXMLLoader(getClass().getResource("MainWindow.fxml"));
+
+        Parent mainWindow = mainWindowLoader.load();
+        MainWindowController mainWindowController = mainWindowLoader.getController();
+
+        stage.setScene(new Scene(mainWindow,800,600));
+        stage.show();
+
     }
 }
